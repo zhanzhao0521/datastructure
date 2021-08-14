cb3aecc519c9d89a3b5fa5bef5cad66da758ddd3
zhanzhao <17634727939@163.com>
2021-08-14 22:57:36 +0800

    upload

--- 0308_SqQueue/SqQueue.c
+++ 0308_SqQueue/SqQueue.c
@@ -36,3 +36,124 @@ Status InitQueue(SqQueue* Q){
 
     return OK;
 }
+
+
+
+/*
+ * 销毁(结构)
+ *
+ * 释放循环顺序队列所占内存。
+ */
+Status DestroyList(SqQueue* Q){
+    if(Q == NULL) return ERROR;
+
+    if((*Q).base)
+        free((*Q).base);
+
+    (*Q).base = NULL;
+    (*Q).front = (*Q).rear = 0;
+    return OK;
+}
+/*
+ * 置空(内容)
+ *
+ * 只是清理循环顺序队列中存储的数据，不释放顺序队列所占内存。
+ */
+Status ClearQueue(SqQueue* Q){
+    if(Q == NULL || (*Q).base == NULL)
+        return ERROR;
+
+    (*Q).front = (*Q).rear = 0;
+
+    return OK;
+}
+
+/*
+ * 判空
+ *
+ * 判断循环顺序队列中是否包含有效数据。
+ *
+ * 返回值：
+ * TRUE : 循环顺序队列为空
+ * FALSE: 循环顺序队列不为空
+ */
+Status QueueEmpty(SqQueue Q){
+    return Q.front == Q.rear ? TRUE : FALSE;
+}
+
+/*
+ * 计数
+ *
+ * 返回循环顺序队列包含的有效元素的数量。
+ */
+int QueueLength(SqQueue Q){
+    if(Q.base == NULL) return ERROR;
+    // 队列长度
+    return (Q.rear - Q.front + MAXQSIZE) % MAXQSIZE;
+}
+
+/*
+ * 取值
+ *
+ * 获取队头元素，将其存储到e中。
+ * 如果可以找到，返回OK，否则，返回ERROR。
+ */
+
+Status GetHead(SqQueue Q,QElemType* e){
+    if(Q.base == NULL || Q.front == Q.rear) return ERROR;
+
+    *e = Q.base[Q.front];
+
+    return OK;
+}
+
+/*
+ * 入队
+ *
+ * 将元素e添加到队列尾部。
+ */
+Status EnQueue(SqQueue* Q,QElemType e){
+    if(Q == NULL || (*Q).base == NULL) return ERROR;
+    // 队列满的标志（会浪费一个空间来区分队列空和队列满）
+    if(((*Q).rear + 1) % MAXQSIZE == (*Q).front) return ERROR;
+
+    (*Q).base[(*Q).rear] = e;
+    (*Q).rear = ((*Q).rear + 1) % MAXQSIZE;
+
+    return OK;
+}
+
+/*
+ * 出队
+ *
+ * 移除队列头部的元素，将其存储到e中。
+ */
+Status DeQueue(SqQueue* Q,QElemType* e){
+    if(Q == NULL || (*Q).base == NULL) return ERROR;
+
+    if((*Q).front == (*Q).rear) return ERROR;
+
+
+    *e = (*Q).base[(*Q).front];
+    // 头指针前进
+    (*Q).front =((*Q).front + 1) % MAXQSIZE;
+
+    return OK;
+}
+/*
+ * 遍历
+ *
+ * 用visit函数访问队列Q
+ */
+Status QueueTraverse(SqQueue Q,void(Visit)(QElemType)){
+    int i;
+
+    if(Q.base == NULL) return ERROR;
+
+    for (i = Q.front; i != Q.rear ; i = (i + 1) % MAXQSIZE)
+        Visit(Q.base[i]);
+
+    printf("\n");
+
+    return OK;
+}
